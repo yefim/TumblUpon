ce96b4c9eefdea3c58260ec107ba1a360f533e66
Ceasar Bautista <cbautista2010@gmail.com>
2012-03-25 02:56:15 -0400

    Fixed logins.

--- tumblupon.py
+++ tumblupon.py
@@ -55,11 +55,11 @@ def get_user_id(username):
 
 @app.before_request
 def before_request():
+    g.db = connect_db()
     g.user = None
     if 'user_id' in session:
         g.user = query_db('select * from user where user_id = ?',
                           [session['user_id']], one=True)
-    g.db = connect_db()
 
 
 @app.teardown_request
@@ -78,6 +78,75 @@ def test01():
 
 def get_tumblr_tag(tag, offset=0):
     return api(TUMBLR, 'tagged', offset=offset, api_key=API_KEY, tag=tag)['response']
+
+# LOGIN
+
+def authenticate(username, password):
+    """Attempt to log the user in. Raises a ValueError on bad credentials."""
+    users = query_db('select * from user')
+    for user in users:
+        if user['username'] == username:
+            if check_password_hash(generate_password_hash(password), password):
+                session['user_id'] = user['user_id']
+                break
+            else:
+                print "bad password"
+                raise ValueError('Invalid password')
+    else:
+        raise ValueError('Invalid username')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        try:
+            authenticate(request.form['username'], request.form['password'])
+        except ValueError as e:
+            return render_template('login.html', error=str(e))
+        else:
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    else:
+        if g.user:
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Registers the user."""
+    if g.user:
+        return redirect(url_for('index'))
+    error = None
+    if request.method == 'POST':
+        if not request.form['username']:
+            error = 'You have to enter a username'
+        elif not request.form['email'] or \
+                 '@' not in request.form['email']:
+            error = 'You have to enter a valid email address'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        elif request.form['password'] != request.form['password2']:
+            error = 'The two passwords do not match'
+        elif get_user_id(request.form['username']) is not None:
+            error = 'The username is already taken'
+        else:
+            g.db.execute('''insert into user (
+                username, email, pw_hash) values (?, ?, ?)''',
+                [request.form['username'], request.form['email'],
+                 generate_password_hash(request.form['password'])])
+            g.db.commit()
+            flash('You were successfully registered and can login now')
+            return redirect(url_for('login'))
+    return render_template('register.html', error=error)
 
 
 # API
