fe8e75caf15206264d934593f6a8ed8edbed8f53
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-03-24 21:16:01 -0400

    changed append to isotope_insert

--- static/javascripts/index.js
+++ static/javascripts/index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
+  $("#container").isotope({
+    itemSelector : '.post',
+    layoutMode : 'fitRows'
+  });
   $.ajax({
     type: 'GET',
     url:'/api/v1/popular'
   }).done(function(response) {
     response = $.parseJSON(response);
     $.each(response, function(index, post) {
-      $('#container').append(render_entry(post));
+      $post = $(render_entry(post));
+      $('#container').isotope('insert', $post);
+      //$('#container').append(render_entry(post));
       console.log(response);
-    });
-    $("#container").isotope({
-      itemSelector : '.post',
-      layoutMode : 'fitRows'
     });
   });
 });
