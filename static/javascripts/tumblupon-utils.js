159b5bc34fb9d484e74fd8c56332e05479de32d5
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-03-24 21:53:53 -0400

    made images size better

--- static/javascripts/tumblupon-utils.js
+++ static/javascripts/tumblupon-utils.js
@@ -17,28 +17,27 @@
  * The content field contains either an image url if type="photo", or 
  * simple text if type="text".
  */
-function render_entry (content_args) {
-    var str = ""; //"<a href='" + content_args.post_url + "'>";
-    str += "<div class='post " + content_args.type + "'>\n";
+
+var MAX_WIDTH = 30;
+var PHOTO_INDEX = 0;
+var ALT_SIZE_INDEX = 0;
+
+render_entry = function(content_args) {
+    // var str = ""; //"<a href='" + content_args.post_url + "'>";
+    //var str += "<div class='post " + content_args.type + "'>";
     switch (content_args.type) {
         case "photo":
-            var photo = content_args.photos[0];
-            str += "\t<img src='" + photo.alt_sizes[photo.alt_sizes.length - 3].url + "'/>\n";
-            break;
+            var photo = content_args.photos[PHOTO_INDEX];
+            return "<div class='post photo' style='background:url("+photo.alt_sizes[ALT_SIZE_INDEX].url+") no-repeat center center'></div>";
         case "text":
-            str += "\t<h1 class='snippet'>" + snip_text(content_args.body) +
-                    "</h1>\n";
-            break;
+            return "<div class='post text'><h1 class='snippet'>" + snip_text(content_args.body) + "</h1></div>";
+        default:
+            return "<div class='post'></div>";
     }
-
-    str += "\t<h2 class='blog-name'>" + content_args.blog_name + "</h2>\n";
-    str += "</div>\n"
-
-    return str;
+    //str += "\t<h2 class='blog-name'>" + content_args.blog_name + "</h2>\n";
 }
 
-function snip_text (string) {
-    var MAX_WIDTH = 30;
+snip_text = function(string) {
     var result = string.replace(/<(?:.|\n)*?>/gm, '');
     return result.substring(0, MAX_WIDTH) + "...";
 }
